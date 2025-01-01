eb00787051b0e6c7ff4a3765df5555a88df271ed
Mirza Azaz <m.azaz3499@gmail.com>
2025-01-01 23:21:39 +0300

    latest changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
       <div className="flex flex-wrap justify-between items-center bg-gradient-to-r from-[#ffffff90] via-[#ffffff30] to-[#ffffff90] border border-white w-[85%] mx-auto max-w-[1180px] py-[5px] px-[20px] rounded-[40px]">
         {/* Logo */}
         <div className="w-[120px] sm:w-[156px]">
-          <img src="/hiringProcess.svg" alt="Header Icon" />
+          <img src={icon} alt="Header Icon" />
         </div>
 
         {/* Desktop Navigation */}
